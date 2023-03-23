ee2edbff2fddb42e4c6810ff2b9019a58aba36f2
blancahidalgo <b.hidalgo.ley@gmail.com>
2023-03-23 20:07:42 +1100

    added bare bones to JS

---
+++ Assets/js/script.js
@@ -0,0 +1,29 @@
+// Variables with API Key's and API Url's
+var geoApiKey = '1813231e63msh4ee73e1d7e35ebfp1ce212jsn92bc31217085';
+var geoApiUrl = 'ip-geo-location.p.rapidapi.com';
+
+var tripAdvisorApiKey = '1813231e63msh4ee73e1d7e35ebfp1ce212jsn92bc31217085';
+var tripAdvisorApiUrl = 'tripadvisor16.p.rapidapi.com';
+
+
+// All parameters will go here
+
+var restoSearch = document.querySelector('location');
+var restoSearch = document.querySelector('');
+var restoSearch = document.querySelector('');
+var restoSearch = document.querySelector('');
+var restoSearch = document.querySelector('');
+var restoSearch = document.querySelector('');
+
+
+// First function here 
+
+function restoFinder(location) {
+
+
+
+
+
+
+    
+}
